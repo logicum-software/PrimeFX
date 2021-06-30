933c125b233f8b9e9577fcf410eb32f6d1df8407
m.gaertner <m.gaertner@CVB0090>
2021-06-30 12:55:24 +0200

    20210630 12:55

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+import java.beans.EventHandler;
+
 public class Controller {
+    public ProgressBar ProgressBarCompute;
+    public Label LabelNumber;
+    public Label LabelDuration;
+    public Button ButtonCompute;
+    public Button ButtonBeenden;
+
+    public void onButtonBeenden(ActionEvent actionEvent) {
+    }
 }
