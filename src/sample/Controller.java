42933f6c3260a78376436a005541219f4d4fb4de
m.gaertner <m.gaertner@CVB0090>
2021-06-30 13:20:38 +0200

    20210630 13:20

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,11 @@
 package sample;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
 
 import java.beans.EventHandler;
 
@@ -14,6 +16,39 @@ public class Controller {
     public Button ButtonCompute;
     public Button ButtonBeenden;
 
+    Task<Integer> task = new Task<Integer>() {
+        @Override protected Integer call() throws Exception {
+            int iterations;
+            for (iterations = 0; iterations < 1000; iterations++) {
+                if (isCancelled()) {
+                    updateMessage("Cancelled");
+                    break;
+                }
+                updateMessage("Iteration " + iterations);
+                updateProgress(iterations, 1000);
+
+                //Block the thread for a short time, but be sure
+                //to check the InterruptedException for cancellation
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException interrupted) {
+                    if (isCancelled()) {
+                        updateMessage("Cancelled");
+                        break;
+                    }
+                }
+            }
+            return iterations;
+        }
+    };
+
     public void onButtonBeenden(ActionEvent actionEvent) {
+        Stage stage = (Stage) ButtonBeenden.getScene().getWindow();
+        stage.close();
+    }
+
+    public void onButtonCompute(ActionEvent actionEvent) {
+        ProgressBarCompute.progressProperty().bind(task.progressProperty());
+        new Thread(task).start();
     }
 }
