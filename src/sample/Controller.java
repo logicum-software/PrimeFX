6d6e76ba13c169c6e70d0d8f2ea8bf61e0d25480
Markus Gärtner <gaertner.brilon@gmail.com>
2021-07-27 14:54:37 +0200

    20210727 14:54

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,9 @@
 package sample;
 
+import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableStringValue;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,8 +16,10 @@ import java.beans.EventHandler;
 import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -29,6 +33,10 @@ public class Controller {
 
     private int nPrimes;
     private boolean IsStarted = false;
+    private Instant StartTime, EndTime;
+    private long Duration;
+    private String DurString = "not calculated";
+    private SimpleStringProperty ObsString = new SimpleStringProperty(DurString);
     private static final List<Integer> Dividends = new ArrayList<>();
     private static final List<Integer> Divisors = new ArrayList<>();
 
@@ -41,6 +49,10 @@ public class Controller {
                 }
                 updateProgress(current, 100000);
                 updateMessage(Integer.toString(nPrimes));
+                EndTime = Instant.now();
+                Duration = ChronoUnit.SECONDS.between(StartTime, EndTime);
+                DurString = String.format("%d", Duration);
+
                 for (int divisor : Divisors) {
                     if (divisor <= current / 2) {
                         if ((float) current % divisor == 0)
@@ -67,8 +79,10 @@ public class Controller {
             task.cancel();
             ButtonCompute.setText("Berechnung starten");
         } else {
+            StartTime = Instant.now();
             ProgressBarCompute.progressProperty().bind(task.progressProperty());
             LabelNumber.textProperty().bind(task.messageProperty());
+            LabelDuration.textProperty().bind(ObsString);
             IsStarted = true;
             ButtonCompute.setText("Abbrechen");
             for (int i = 3; i <= 100000; i++)
